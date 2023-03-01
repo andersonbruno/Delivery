811ab419f894e294c281a71013dee7bf28c0d87c
Anderson Bruno <andersonblsantos@Outlook.com>
2023-03-01 20:49:15 -0300

    Created item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,5 +1,32 @@
-export default function Item () {
+import './Item.scss';
+
+interface ItemProps {
+    name: string;
+    description: string;
+    image: string;
+    price: string;
+}
+
+export default function Item (props: ItemProps) {
+    const {name, description, image, price } = props;
     return (
-        <div>Somethign</div>
+        <div className='item-container grow'>
+            <div className='items-infos'>
+                <div className='item-title'>
+                    <h3>{name}</h3>
+                </div>
+                <div className='item-info'>
+                    <div className='item-description'>
+                        {description}
+                    </div>
+                </div>
+                <div className='item-price'>
+                    R$ {price}
+                </div>
+            </div>
+            <div className='item-image'>
+                <img src={image} alt='Churrasco'/>
+            </div>
+        </div>
     );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,16 +4,65 @@ import bonarabe from '../../assets/itens/bonarabe.png';
 import arnaldo from '../../assets/itens/arnaldo.jpg';
 import mc from '../../assets/itens/MC.jpg';
 import ouroroxo from '../../assets/itens/ouroroxo.jpg';
+import Item from '../../components/Item';
+import churrasco from '../../assets/itens/churrasco.png';
+import parmegiana from '../../assets/itens/parmegiana.png';
+import spaten from '../../assets/itens/spaten.jpg';
 
 export default function Home () {
     return (
         <div className='header'>
             <h3>Lojas</h3>
             <div className='stores'>
-                <Store name='Churrascaria do arnaldo' note='5.0' timeToDeliver='30-40' category='Brasileira' image={arnaldo}/>
-                <Store name='Bonárabe - Parnamirim' note='4.5' timeToDeliver='30-40' category='Lanches' image={bonarabe}/>
-                <Store name="Mcdonald's" note='4.9' timeToDeliver='30-40' category='Lanches' image={mc}/>
-                <Store name='Ouro Roxo Açaíteria' note='3.5' timeToDeliver='30-40' category='Lanches' image={ouroroxo}/>
+                <Store 
+                    name='Churrascaria do arnaldo' 
+                    note='5.0' 
+                    timeToDeliver='30-40' 
+                    category='Brasileira' 
+                    image={arnaldo}
+                />
+                <Store 
+                    name='Bonárabe - Parnamirim' 
+                    note='4.5' 
+                    timeToDeliver='30-40' 
+                    category='Lanches' 
+                    image={bonarabe}
+                />
+                <Store 
+                    name="Mcdonald's" 
+                    note='4.9' 
+                    timeToDeliver='30-40' 
+                    category='Lanches' 
+                    image={mc}
+                />
+                <Store 
+                    name='Ouro Roxo Açaíteria' 
+                    note='3.5' 
+                    timeToDeliver='30-40' 
+                    category='Lanches' 
+                    image={ouroroxo}
+                />
+            </div>
+            <h3>Produtos</h3>
+            <div className='items'>
+                <Item 
+                    name='Combo mistão (serve 4 pessoas) + Guaraná 2litros' 
+                    description='Carne, frango e linguiça. Acompanha arroz de leite, feijão verde, farofa, Vinagrete, macaxeira cozida, macarrão, pirão de queijo.'
+                    image={churrasco}
+                    price='138,00'
+                />
+                <Item 
+                    name='Combo parmegiana (serve 4 pessoas) + Guaraná 2 litros' 
+                    description='Filé de carne ou frango empanado. Acompanha macarrão ao molho de tomate.'
+                    image={parmegiana}
+                    price='128,00'
+                />
+                <Item 
+                    name='Spaten log nek 10 unidade' 
+                    description='Spaten'
+                    image={spaten}
+                    price='105,50'
+                />
             </div>
         </div>
     )
