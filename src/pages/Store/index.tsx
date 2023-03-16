6919028403c852ac0d93605884700999b6d30422
Anderson Bruno <andersonblsantos@Outlook.com>
2023-03-16 16:00:33 -0300

    Improved the sass class names and renamed the scss files

---
+++ src/pages/Store/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Store.module.scss';
+
+export default function Store () {
+    return (
+        <div className={styles.container}>
+            <h1>Something</h1>
+        </div>    
+    );
+}
